13b14f702ef2ad3628466eed72b947b98cd055d7
Brutalketcap <ozanates22@gmail.com>
2025-12-03 06:44:52 +0300

    update

--- InsureYouAI/Controllers/ArticleController.cs
+++ InsureYouAI/Controllers/ArticleController.cs
@@ -1,6 +1,10 @@
-﻿using InsureYouAI.Context;
+﻿using InsureYouAI.ApiKey;
+using InsureYouAI.Context;
 using InsureYouAI.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
 
 namespace InsureYouAI.Controllers
 {
@@ -66,5 +70,51 @@ namespace InsureYouAI.Controllers
 
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateArticleWithOpenAI(string prompt)
+        {
+            var apiKey = ApiKeysOnly.OpenAIAPIKey;
+
+            using var clinet = new HttpClient();
+            clinet.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+
+            var requestDate = new
+            {
+                model = "gpt-4o-mini",
+                messages = new[]
+                {
+                    new { role="system", content="sen bir sigorta şirketi için çalışan, içerik yazarlığı yapan bir yapay zekasın.Kullanıcının verdiği özet ve anaktar kelimelerine göre, sigortacılık sektörüyle ilgili makele üret. en Fazla 500 karakterli" },
+                    new { role="user", content=prompt}
+                },
+                temperature = 0.7
+            };
+            var response = await clinet.PostAsJsonAsync("https://api.openai.com/v1/chat/completions", requestDate);
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<OpenAIResponse>();
+                var content = result.choices[0].message.content;
+                ViewBag.article= content;
+            }
+            else
+            {
+                ViewBag.article= "Bir hata oluştu: " + response.StatusCode;
+            }
+            return View();
+        }
+        public class OpenAIResponse
+        {
+            public List<Choice> choices { get; set; }
+        }
+        public class Choice
+        {
+            public Message message { get; set; }
+        }
+        public class Message
+        {
+            public string role { get; set; }
+            public string content { get; set; }
+        }
     }
 }
+     
